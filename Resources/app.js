d1dc6d7c4d1e2d4ac2cb365ad3a203c9f370c2de
WillR <w.ransom84@gmail.com>
2023-07-12 21:26:16 +0100

    start js carousel image

--- Resources/app.js
+++ Resources/app.js
@@ -4,3 +4,12 @@ const navLinks = document.getElementsByClassName('navlinks')[0];
 hamburgerMenu.addEventListener('click', () =>{
     navLinks.classList.toggle('active')
 })
+
+document.querySelectorAll(".carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".carousel-item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel-button></span>`;
+    })
+});
+
+/*11 12 */
